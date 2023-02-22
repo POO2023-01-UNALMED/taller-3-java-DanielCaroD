4e6e4bba7c6f5bc589be7ab9912809d357520c8e
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-02-22 13:15:01 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,14 @@
+package taller3.televisores;
+
+public class Marca {
+	String nombre;
+	
+	public void setNombre(String str) {
+		nombre = str;
+	
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+}
