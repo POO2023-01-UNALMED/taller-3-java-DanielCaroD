418991ef4961e2a13fd4895eaf0b777d0c42483e
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-02-26 12:59:33 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,12 +1,12 @@
 package taller3.televisores;
 
 public class TV {
-	Marca marca;
-	int canal = 1;
-	int precio = 500;
-	boolean estado;
-	int volumen = 1;
-	Control control;
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	private boolean estado;
+	private int volumen = 1;
+	private Control control;
 	private static int numTV = 0;
 	
 	public TV(Marca marca, boolean estado) {
