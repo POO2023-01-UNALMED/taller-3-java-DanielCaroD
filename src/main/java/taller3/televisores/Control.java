7e0d98cc27890b998dbdbad99d439ab670925f7f
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-02-25 20:58:52 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -4,10 +4,51 @@ public class Control {
 	TV tv;
 	
 	public void turnOn() {
-		tv.turnOn(true);
+		this.tv.estado = true;
 	}
 	
 	public void turnOff() {
-		tv.turnOff(false);
+		this.tv.estado = false;
+	}
+	
+	public void canalUp() {
+		if (this.tv.estado == true && this.tv.canal >= 1 && this.tv.canal <120) {
+			this.tv.canal += 1; }
+	}
+	
+	public void canalDown() {
+		if (this.tv.estado == true && this.tv.canal > 1 && this.tv.canal <=120) {
+			this.tv.canal -= 1; }
+	}
+	
+	public void volumenUp() {
+		if (this.tv.estado == true && this.tv.volumen >= 0 && this.tv.volumen < 7) {
+					this.tv.volumen += 1;
+		}
+	}
+	
+	public void volumenDown() {
+		if (this.tv.estado == true && this.tv.volumen > 0 && this.tv.volumen <= 7) {
+					this.tv.volumen -= 1;
+		}
+	}
+	
+	public void setCanal(int canal) {
+		if (canal >= 1 && canal <=120) {
+			this.tv.canal = canal;
+		}
+	}
+	
+	public void enlazar(TV tv) {
+		this.tv = tv;
+		tv.setControl(this);
+	}
+	
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+	
+	public TV getTv() {
+		return tv;
 	}
 }
